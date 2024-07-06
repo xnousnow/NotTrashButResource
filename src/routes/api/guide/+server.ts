4f50908b55cbbdd62c7511031407e4981d8c9e14
xnousnow <xnou.snow@gmail.com>
2024-07-06 21:17:32 +0900

    `/api/guide` 완성

--- src/routes/api/guide/+server.ts
+++ src/routes/api/guide/+server.ts
@@ -17,7 +17,7 @@ export const POST = (async ({ request }) => {
       id: 'bl',
       name: '스펀지',
       guide: '일반 쓰레기로 배출해요.',
-      tips: 'PS가 아닌 재질에 공기를 넣어 만들었기 때문이예요.'
+      tips: 'PS가 아닌 재질에 공기를 넣어 만들었기 때문이에요.'
     },
     { id: 'je', name: '고무', guide: '일반 쓰레기로 배출해요.' },
     {
@@ -44,7 +44,7 @@ export const POST = (async ({ request }) => {
       id: 'rm',
       name: '아이스팩',
       guide:
-        '아이스팩 고흡수성 수지(젤 형태)가 포함된 아이스팩은 일반 쓰레기로 배출해요. 물 아이스팩은 내용물을 모두 비우면 비닐로 배출할 수 있어요.',
+        '고흡수성 수지(젤 형태)가 포함된 아이스팩은 일반 쓰레기로 배출해요. 물 아이스팩은 내용물을 모두 비우고 비닐로 배출할 수 있어요.',
       tips: '아이스팩 겉면에 고흡수성 수지 포함 여부가 표기되어 있어요.'
     },
     {
@@ -56,15 +56,15 @@ export const POST = (async ({ request }) => {
   ]
   const names = guidebook.map(({ name }) => name)
 
-  let prompt = `다음 중 이미지의 물건을 가장 잘 나타내는 항목을 고르세요. \`result\`에는 다음 항목에 있는 물건만 포함하세요. 모든 답변은 한국어로 작성하세요.\n\n> ${names.join(', ')}`
-  console.log(prompt)
+  const identifyPrompt = `다음 중 이미지의 물건을 가장 잘 나타내는 항목을 고르세요. \`result\`에는 다음 항목에 있는 물건만 포함하세요. 모든 답변은 한국어로 작성하세요.\n\n> ${names.join(', ')}`
+  console.log(identifyPrompt)
 
-  const result = await generateObject({
+  const identifyResult = await generateObject({
     model: openai('gpt-4o'),
     messages: [
       {
         role: 'system',
-        content: prompt
+        content: identifyPrompt
       },
       {
         role: 'user',
@@ -78,12 +78,10 @@ export const POST = (async ({ request }) => {
     ],
     schema: z.object({
       result: z
-        .array(
-          z.string().describe('항목의 정확한 이름, 항목이 없다면 최대한 가까운 항목을 고르세요')
-        )
+        .array(z.string().describe('이미지의 물건을 가장 잘 나타내는 항목'))
         .max(2)
         .optional(),
-      identified: z.string().describe('이미지의 물건'),
+      identified: z.string().describe('항목과 상관없이 이미지의 물건'),
       issues: z
         .object({
           multipleObjects: z.boolean().optional().describe('이미지 중심에 여러 물건이 있음'),
@@ -94,5 +92,56 @@ export const POST = (async ({ request }) => {
     })
   })
 
-  return new Response(JSON.stringify(result))
+  if (identifyResult.object.issues && Object.keys(identifyResult.object.issues).length > 0) {
+    return new Response(JSON.stringify(identifyResult.object))
+  }
+
+  const selectedGuides = guidebook.filter(({ name }) =>
+    identifyResult.object.result?.includes(name)
+  )
+  console.log(identifyResult.object)
+  console.log(selectedGuides)
+  const markdownGuides = selectedGuides.map(({ name, guide, tips }) => {
+    return `# ${name}\n\n${guide}\n\n${tips ? `## 팁\n${tips}` : ''}`
+  })
+  const finalPrompt =
+    [
+      '다음 자료를 참고하여 이미지의 물건을 분리배출하는 방법을 안내하세요.',
+      '- 문장에는 해요체 (해요, 예요)를 사용하고 마침표를 찍으세요.',
+      '- 자료를 똑같이 사용하지 말고 **이미지에 맞춰 단계와 팁을 수정**하세요. 예시:',
+      ' - 이미지 물건이 이미 세척되어 있다면 세척 단계를 제거합니다.',
+      ' - 라벨이 없다면 라벨 단계를 제거합니다.',
+      ' - 물건과 상관없는 팁을 제거합니다.',
+      '',
+      markdownGuides.join('\n\n')
+    ].join('\n')
+  console.log(finalPrompt)
+
+  const result = await generateObject({
+    model: openai('gpt-4o'),
+    messages: [
+      {
+        role: 'system',
+        content: finalPrompt
+      },
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'image',
+            image
+          }
+        ]
+      }
+    ],
+    schema: z.object({
+      name: z.string().describe('물건의 이름, 1~3 단어 이내로 작성'),
+      guide: z
+        .array(z.string().describe('한 문장 이내'))
+        .describe('단계별 분리배출 안내, 필요없는 단계는 빼세요'),
+      tips: z.array(z.string().describe('한 문장 이내')).optional().describe('추가 팁')
+    })
+  })
+
+  return new Response(JSON.stringify(result.object))
 }) satisfies RequestHandler
