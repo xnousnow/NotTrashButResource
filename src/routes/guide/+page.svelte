a458ec5c9c61de362c09c50749a2b93925455240
xnousnow <xnou.snow@gmail.com>
2024-07-14 14:22:42 +0900

    분리배출 방법 스켈레톤 로더 컴포넌트로 분리

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -5,7 +5,7 @@
   import { image } from '../../stores'
   import ArrowBack from '~icons/material-symbols/ArrowBack'
   import AutoAwesome from '~icons/material-symbols/AutoAwesome'
-  import CloudOff from '~icons/material-symbols/CloudOff'
+  import ResultSkeletonLoader from '../../components/ResultSkeletonLoader.svelte'
   import ResultDisplay from '../../components/ResultDisplay.svelte'
   import IssuesDisplay from '../../components/IssuesDisplay.svelte'
 
@@ -111,21 +111,7 @@
   {/if}
   <div class="relative">
     {#if generating}
-      <div class="absolute top-0 left-0 space-y-2" transition:blur={{ duration: 300 }}>
-        <div class="my-1 h-10 w-48 animate-pulse rounded-xl bg-white/30 pl-1"></div>
-        {#each ['w-48', 'w-32', 'w-40'] as width}
-          <div class="flex gap-2">
-            <div class="h-7 w-8 animate-pulse rounded-full bg-white/30 shrink-0"></div>
-            <div class="{width} h-7 animate-pulse rounded-lg bg-white/30"></div>
-          </div>
-        {/each}
-        {#each ['w-56', 'w-32'] as width}
-          <div class="flex items-center gap-2">
-            <div class="mx-1 h-2 w-6 animate-pulse rounded-full bg-white/30 shrink-0"></div>
-            <div class="{width} h-7 animate-pulse rounded-lg bg-white/30"></div>
-          </div>
-        {/each}
-      </div>
+      <ResultSkeletonLoader />
     {:else if error || response.issues}
       <IssuesDisplay response={response} error={error} />
     {:else}
