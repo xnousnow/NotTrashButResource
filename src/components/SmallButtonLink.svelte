f4d1f0d4fbee2514a36a9254517d297fcce89a9d
xnousnow <xnou.snow@gmail.com>
2024-07-14 18:41:37 +0900

    코드 포맷

--- src/components/SmallButtonLink.svelte
+++ src/components/SmallButtonLink.svelte
@@ -4,7 +4,10 @@
   export let href: string
 </script>
 
-<a {href} class="mt-2 flex shrink justify-center gap-1 rounded-full bg-white/20 py-1 pl-2 pr-3 hover:bg-white/30 duration-200">
+<a
+  {href}
+  class="mt-2 flex shrink justify-center gap-1 rounded-full bg-white/20 py-1 pl-2 pr-3 duration-200 hover:bg-white/30"
+>
   <Icon class="h-6 w-6" />
   {text}
 </a>
