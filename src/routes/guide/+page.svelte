fc552013c09d890161d96853c8a78bdf67e72307
xnousnow <xnou.snow@gmail.com>
2024-07-14 14:13:11 +0900

    분리배출 방법 컴포넌트로 분리

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -12,6 +12,7 @@
   import Description from '~icons/material-symbols/Description'
   import Reply from '~icons/material-symbols/Reply'
   import SmallButtonLink from '../../components/SmallButtonLink.svelte'
+  import ResultDisplay from '../../components/ResultDisplay.svelte'
 
   const resizeImage = (file: File, maxWidth: number, maxHeight: number) => {
     return new Promise<string>((resolve, reject) => {
@@ -157,31 +158,7 @@
         <SmallButtonLink Icon={Reply} text="돌아가기" href='/' />
       </div>
     {:else}
-      <div class="absolute top-0 left-0 space-y-2" transition:blur={{ duration: 300 }}>
-        <h1 class="my-1 pl-1 text-4xl font-bold">{response.name}</h1>
-        <ul class="space-y-2">
-          {#each response.guide as step, i}
-            <li class="flex items-center gap-2">
-              <span
-                class="flex w-8 items-center justify-center rounded-full bg-white/20 p-0.5 font-semibold shrink-0"
-              >
-                {i + 1}
-              </span>
-              <p class="inline">{step}</p>
-            </li>
-          {/each}
-        </ul>
-        {#if response.tips}
-          <ul class="mt-2 space-y-2">
-            {#each response.tips as tip}
-              <li class="flex items-center gap-2">
-                <div class="mx-1 h-2 w-6 rounded-full bg-white/20 shrink-0"></div>
-                <p class="inline h-7">{tip}</p>
-              </li>
-            {/each}
-          </ul>
-        {/if}
-      </div>
+      <ResultDisplay response={response} />
     {/if}
   </div>
 </div>
