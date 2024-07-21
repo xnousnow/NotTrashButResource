98ad765eab1c65b835ddf9010acc416bd3c71004
xnousnow <xnou.snow@gmail.com>
2024-07-21 17:46:12 +0900

    오류 처리 개선

--- src/components/IssuesDisplay.svelte
+++ src/components/IssuesDisplay.svelte
@@ -23,6 +23,12 @@
   {#if error}
     <CloudOff class="mx-auto h-16 w-16" />
     <p>결과를 불러오는 데 실패했어요.<br />나중에 다시 시도해 보세요.</p>
+    {:else if response.issues.noMatch || !response.result}
+    <Description class="mx-auto h-16 w-16" />
+    <p>
+      <span class="rounded bg-white/20 px-1 py-0.5">{response.identified}</span>이(가) 인식되었지만<br
+    />정확한 정보를 찾지 못했어요.
+    </p>
   {:else if Object.keys(response.issues).length === 0}
     <BrokenImage class="mx-auto h-16 w-16" />
     <p>사진에 여러 문제가 있어요.</p>
@@ -32,12 +38,6 @@
   {:else if response.issues.noObject}
     <ViewInArOff class="mx-auto h-16 w-16" />
     <p>사진에 물체가 없어요.</p>
-  {:else if response.issues.noMatch}
-    <Description class="mx-auto h-16 w-16" />
-    <p>
-      <span class="rounded bg-white/20 px-1 py-0.5">{response.identified}</span>이(가) 인식되었지만<br
-      />정확한 정보를 찾지 못했어요.
-    </p>
   {:else}
     <SmartToy class="mx-auto h-16 w-16" />
     <p>기타 오류가 발생했어요.</p>
