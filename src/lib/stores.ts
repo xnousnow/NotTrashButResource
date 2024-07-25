da5629d657f085c05d6a39b5c4ed8b6ae81968f7
xnousnow <xnou.snow@gmail.com>
2024-07-25 15:45:21 +0900

    앱 첫 방문 시 `/onboarding` 표시

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,14 @@
 import { writable } from 'svelte/store'
 
 export const image = writable<File | null>(null)
+export const didOnboarding = writable(false)
+
+if (typeof window !== 'undefined') {
+  if (localStorage.getItem('didOnboarding')) {
+    didOnboarding.set(true)
+  }
+
+  didOnboarding.subscribe(value => {
+    localStorage.setItem('didOnboarding', value ? 'true' : 'false')
+  })
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
   import '../app.css'
+  import { image, didOnboarding } from '$lib/stores'
+  import { onMount } from 'svelte'
+  import { goto } from '$app/navigation'
+
+  onMount(() => {
+    if (!$image) goto('/')
+    if ($didOnboarding) goto('/onboarding')
+    console.log($didOnboarding)
+  })
 </script>
 
 <div class="relative h-[100dvh] w-full bg-black text-white">
