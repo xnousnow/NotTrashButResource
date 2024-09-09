81e7525fb7afb2d3f916d22e77fd9a47e9183acc
xnousnow <xnou.snow@gmail.com>
2024-09-09 18:34:18 +0900

    `resizeImage` 함수 분리

--- src/lib/useAPI.ts
+++ src/lib/useAPI.ts
@@ -1,34 +1,5 @@
 import type { ObjectResponseData, GuideResponseData, ErrorResponseData } from '$api/guide/types'
-
-const resizeImage = (file: File, maxWidth: number, maxHeight: number) =>
-  new Promise<string>((resolve, reject) => {
-    if (!(file instanceof Blob))
-      return reject(new TypeError('The provided value is not a Blob or File.'))
-
-    const reader = new FileReader()
-    reader.onload = () => {
-      const img = new Image()
-      img.onload = () => {
-        const canvas = document.createElement('canvas')
-        let { width, height } = img
-
-        if (width > maxWidth || height > maxHeight) {
-          const aspectRatio = width / height
-          width = aspectRatio > 1 ? maxWidth : Math.round(maxHeight * aspectRatio)
-          height = aspectRatio > 1 ? Math.round(maxWidth / aspectRatio) : maxHeight
-        }
-
-        canvas.width = width
-        canvas.height = height
-        canvas.getContext('2d')?.drawImage(img, 0, 0, width, height)
-        resolve(canvas.toDataURL())
-      }
-      img.onerror = reject
-      img.src = reader.result as string
-    }
-    reader.onerror = reject
-    reader.readAsDataURL(file)
-  })
+import { processImage } from './processImage'
 
 export const useAPI = async (
   image: File,
@@ -39,7 +10,7 @@ export const useAPI = async (
   close: () => void
 ) => {
   try {
-    const processedImage = await resizeImage(image, 200, 200)
+    const processedImage = await processImage(image, 200, 200)
 
     const response = await fetch('/api/guide', {
       method: 'POST',
