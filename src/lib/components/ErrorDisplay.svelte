fb7b079e6297a8b3c2d4792f6536055129b42500
xnousnow <xnou.snow@gmail.com>
2024-09-16 11:05:04 +0900

    오류 문구 변경

--- src/lib/components/ErrorDisplay.svelte
+++ src/lib/components/ErrorDisplay.svelte
@@ -11,6 +11,8 @@
   import ViewInArOff from '~icons/material-symbols/ViewInArOff'
 
   import SmallButton from '$components/SmallButton.svelte'
+  
+  import { inputMode } from '$lib/stores'
 
   import type { ErrorResponseData as ImageErrorResponseData } from '$api/guide/types'
   import type { ErrorResponseData as TextErrorResponseData } from '$api/text-guide/types'
@@ -29,7 +31,11 @@
     <p>사진에 물건이 없어요.</p>
   {:else if error.errors?.noMatches}
     <Description class="mx-auto h-16 w-16" />
-    <p>물건{usePlural ? '들' : ''}을 인식했지만<br />정확한 정보를 찾지 못했어요.</p>
+    {#if $inputMode === 'image'}
+      <p>물건{usePlural ? '들' : ''}을 인식했지만<br />정확한 정보를 찾지 못했어요.</p>
+    {:else}
+      <p>정확한 정보를 찾지 못했어요.</p>
+    {/if}
   {:else if error.errors?.imageError}
     <BrokenImage class="mx-auto h-16 w-16" />
     <p>물건을 인식할 수 없어요.<br />물건이 잘 보이도록 다시 찍어주세요.</p>
